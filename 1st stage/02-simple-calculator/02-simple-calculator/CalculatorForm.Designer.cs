9ff13d1338682d91e2dff06c396183df43662ccf
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-11-07 20:36:33 -0300

    feat: addition of the simple calculator project

---
+++ 1st stage/02-simple-calculator/02-simple-calculator/CalculatorForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace _02_simple_calculator
+{
+    partial class CalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber1 = new System.Windows.Forms.Label();
+            this.lblNumber2 = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.btnAddition = new System.Windows.Forms.Button();
+            this.btnSubtraction = new System.Windows.Forms.Button();
+            this.btnMultiplication = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumber1
+            // 
+            this.lblNumber1.AutoSize = true;
+            this.lblNumber1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber1.ForeColor = System.Drawing.Color.White;
+            this.lblNumber1.Location = new System.Drawing.Point(12, 79);
+            this.lblNumber1.Name = "lblNumber1";
+            this.lblNumber1.Size = new System.Drawing.Size(138, 16);
+            this.lblNumber1.TabIndex = 0;
+            this.lblNumber1.Text = "Digite o 1º número:";
+            // 
+            // lblNumber2
+            // 
+            this.lblNumber2.AutoSize = true;
+            this.lblNumber2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber2.ForeColor = System.Drawing.Color.White;
+            this.lblNumber2.Location = new System.Drawing.Point(12, 128);
+            this.lblNumber2.Name = "lblNumber2";
+            this.lblNumber2.Size = new System.Drawing.Size(138, 16);
+            this.lblNumber2.TabIndex = 1;
+            this.lblNumber2.Text = "Digite o 2º número:";
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber1.Location = new System.Drawing.Point(173, 76);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(75, 22);
+            this.txtNumber1.TabIndex = 2;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber2.Location = new System.Drawing.Point(173, 125);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(75, 22);
+            this.txtNumber2.TabIndex = 3;
+            // 
+            // btnAddition
+            // 
+            this.btnAddition.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnAddition.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddition.ForeColor = System.Drawing.Color.White;
+            this.btnAddition.Location = new System.Drawing.Point(15, 230);
+            this.btnAddition.Name = "btnAddition";
+            this.btnAddition.Size = new System.Drawing.Size(105, 48);
+            this.btnAddition.TabIndex = 4;
+            this.btnAddition.Text = "+";
+            this.btnAddition.UseVisualStyleBackColor = false;
+            this.btnAddition.Click += new System.EventHandler(this.btnAddition_Click);
+            // 
+            // btnSubtraction
+            // 
+            this.btnSubtraction.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnSubtraction.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtraction.ForeColor = System.Drawing.Color.White;
+            this.btnSubtraction.Location = new System.Drawing.Point(143, 230);
+            this.btnSubtraction.Name = "btnSubtraction";
+            this.btnSubtraction.Size = new System.Drawing.Size(105, 48);
+            this.btnSubtraction.TabIndex = 5;
+            this.btnSubtraction.Text = "-";
+            this.btnSubtraction.UseVisualStyleBackColor = false;
+            this.btnSubtraction.Click += new System.EventHandler(this.btnSubtraction_Click);
+            // 
+            // btnMultiplication
+            // 
+            this.btnMultiplication.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnMultiplication.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplication.ForeColor = System.Drawing.Color.White;
+            this.btnMultiplication.Location = new System.Drawing.Point(15, 312);
+            this.btnMultiplication.Name = "btnMultiplication";
+            this.btnMultiplication.Size = new System.Drawing.Size(105, 48);
+            this.btnMultiplication.TabIndex = 6;
+            this.btnMultiplication.Text = "x";
+            this.btnMultiplication.UseVisualStyleBackColor = false;
+            this.btnMultiplication.Click += new System.EventHandler(this.btnMultiplication_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnDivision.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivision.ForeColor = System.Drawing.Color.White;
+            this.btnDivision.Location = new System.Drawing.Point(143, 312);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(105, 48);
+            this.btnDivision.TabIndex = 7;
+            this.btnDivision.Text = "÷";
+            this.btnDivision.UseVisualStyleBackColor = false;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // CalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(260, 409);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnMultiplication);
+            this.Controls.Add(this.btnSubtraction);
+            this.Controls.Add(this.btnAddition);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.lblNumber2);
+            this.Controls.Add(this.lblNumber1);
+            this.Name = "CalculatorForm";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumber1;
+        private System.Windows.Forms.Label lblNumber2;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Button btnAddition;
+        private System.Windows.Forms.Button btnSubtraction;
+        private System.Windows.Forms.Button btnMultiplication;
+        private System.Windows.Forms.Button btnDivision;
+    }
+}
+
---
+++ 1st stage/02-simple-calculator/02-simple-calculator/CalculatorForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_simple_calculator
+{
+    public partial class CalculatorForm : Form
+    {
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddition_Click(object sender, EventArgs e)
+        {
+            float number1 = float.Parse(txtNumber1.Text);
+            float number2 = float.Parse(txtNumber2.Text);
+            float addition;
+
+            addition = number1 + number2;
+
+            MessageBox.Show(number1 + " + " + number2 + " = " + addition);
+        }
+
+        private void btnSubtraction_Click(object sender, EventArgs e)
+        {
+            float number1 = float.Parse(txtNumber1.Text);
+            float number2 = float.Parse(txtNumber2.Text);
+            float subtraction;
+
+            subtraction = number1 - number2;
+
+            MessageBox.Show(number1 + " - " + number2 + " = " + subtraction);
+        }
+
+        private void btnMultiplication_Click(object sender, EventArgs e)
+        {
+            float number1 = float.Parse(txtNumber1.Text);
+            float number2 = float.Parse(txtNumber2.Text);
+            float multiplication;
+
+            multiplication = number1 * number2;
+
+            MessageBox.Show(number1 + " x " + number2 + " = " + multiplication);
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            float number1 = float.Parse(txtNumber1.Text);
+            float number2 = float.Parse(txtNumber2.Text);
+            float division;
+
+            division = number1 / number2;
+
+            MessageBox.Show(number1 + " ÷ " + number2 + " = " + division);
+        }
+    }
+}
