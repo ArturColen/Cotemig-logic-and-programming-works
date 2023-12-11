b8a35cf52cad257625b9226e76a15d1017a19ad1
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-12-11 16:22:28 -0300

    feat: addition of the matrix filling project

---
+++ 3rd stage/17-matrix-filling/17-matrix-filling/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace _17_matrix_filling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFillMatrix = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFillMatrix
+            // 
+            this.btnFillMatrix.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnFillMatrix.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFillMatrix.ForeColor = System.Drawing.Color.White;
+            this.btnFillMatrix.Location = new System.Drawing.Point(12, 43);
+            this.btnFillMatrix.Name = "btnFillMatrix";
+            this.btnFillMatrix.Size = new System.Drawing.Size(142, 38);
+            this.btnFillMatrix.TabIndex = 0;
+            this.btnFillMatrix.Text = "Preencher matriz";
+            this.btnFillMatrix.UseVisualStyleBackColor = false;
+            this.btnFillMatrix.Click += new System.EventHandler(this.btnFillMatrix_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 154);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 16);
+            this.lblResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnFillMatrix);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Exercício - Preenchimento de matriz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFillMatrix;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ 3rd stage/17-matrix-filling/17-matrix-filling/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _17_matrix_filling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[,] numberMatrix = new int[3, 3];
+
+        private void btnFillMatrix_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    string value = Interaction.InputBox("Insira nove elementos e obtenha uma matriz 3x3", "Matrizes");
+                    numberMatrix[i, j] = int.Parse(value);
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    lblResult.Text += numberMatrix[i, j] + " ";
+                }
+
+                lblResult.Text += "\n";
+            }
+        }
+    }
+}
